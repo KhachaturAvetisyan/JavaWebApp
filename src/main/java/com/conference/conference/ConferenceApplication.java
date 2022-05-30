f794e6ab6281cc174215471edd25a946afd0d639
Khachatur Avetisyan <avetisyan.khachatur@student.rau.am>
2022-05-30 19:21:03 +0400

    add home page and controllers

--- src/main/java/com/conference/conference/ConferenceApplication.java
+++ src/main/java/com/conference/conference/ConferenceApplication.java
@@ -2,12 +2,15 @@ package com.conference.conference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"controllers"})
 public class ConferenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceApplication.class, args);
+		System.out.println("start");
 	}
 
 }
