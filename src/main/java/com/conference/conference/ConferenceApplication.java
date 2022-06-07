bb131492f69ab2d8f14f21ba5e27bbec0419772a
Khachatur Avetisyan <avetisyan.khachatur@student.rau.am>
2022-06-07 11:27:39 +0400

    add header footer

--- src/main/java/com/conference/conference/ConferenceApplication.java
+++ src/main/java/com/conference/conference/ConferenceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"controllers"})
+//@ComponentScan(basePackages = {"com/conference/conference/controllers"})
 public class ConferenceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/conference/conference/controllers/MainController.java
+++ src/main/java/com/conference/conference/controllers/MainController.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.conference.conference.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
